5c3062ce4be96ff39d584833a750234505f719a1
RafaelHuerta1 <010530713@alumno.utel.edu.mx>
2024-05-09 22:31:12 -0700

    Refactor code and add lazy loading for images, update dependencies, and improve navigation

--- app/index.js
+++ app/index.js
@@ -9,12 +9,30 @@ import Welcome from './pantallas/Welcome';
 //import	 {useAuth} from "./hooks/userAuth";
 import TabLayout from './pantallas/_layout';
 import Tab from './(tabs)/_layout';
+import * as Updates from 'expo-updates';
 
-
+import { useEffect } from 'react';
 // signin
 // signup
 export default function index() {
  
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
+  useEffect(() => {
+    onFetchUpdateAsync();
+  }, []);
 
 
 
